95385f7e8763712b8820aae10d0651191ad5132f
unknown <kyle.rosentrater@bryanuniversity.edu>
2024-04-05 16:00:35 -0700

    fixed typo in app.js

--- budget-manager/src/App.js
+++ budget-manager/src/App.js
@@ -1,6 +1,19 @@
+import { useContext } from 'react';
+import { UserContext } from './context/UserProvider.js';
+import { Routes, Route, Navigate } from 'react-dom';
+
 
 
 function App() {
+
+  const { token } = useContext(UserContext);
+  const shouldRedirect = true;
+
+  function requiredAuth({ children, redirectTo }){
+    let isAuthenticated = token;
+    return isAuthenticated ? children : <Navigate to = {redirectTo} />;
+  };
+
   return (
     <>
 
